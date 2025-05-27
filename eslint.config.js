09a438c8dc95e966fdb7ee058851abdb40098fd3
onamonapia <mirrundell@gmail.com>
2025-05-26 20:41:55 -0700

    husky, eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const prettier = require("eslint-plugin-prettier");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      semi: ["warn", "always"],
+    },
   },
 ]);
