adb256b0d5e5e4a47f4c158567e9dccc38eaf7ef
MiltonVuong <miltonvuong@live.com>
2025-05-29 10:40:47 -0700

    Forecast Screen Update

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## week 1 stuff to do:
 
 ### GitHub Repository & Project Board Setup
+
 - [x] GitHub Repo
 - [x] React Native + Expo app
 - [ish] ESLint and Prettier enabled
@@ -18,16 +19,17 @@
 - [x] tasks linked to issues where applicable
 
 ### Software Development Plan & Wireframes
+
 - [ish] SOFTWARE_DEVELOPMENT_PLAN.md in the root of the repo
 - [x] /wireframes folder
 - [x] app screen wireframes
 - [x] user flow diagrams
 
   In the .md file:
+
   - [x] team member names + assigned roles
   - [ish] problem the app will solve
   - [ish] target users and user personas
   - [ish] mvp feature list
   - [ish] planned tech stack and third-party libraries
   - [ish] sprint plan or timeline overview
-  
---
+++ app/(tabs)/forecast.tsx
@@ -0,0 +1,49 @@
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import TaskCard from '@/components/TaskCard';
+
+export default function ForecastScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Forecast</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <TaskCard taskName="Vacuum Living Room" timeRemaining="3 days" />
+        <TaskCard taskName="Buy Paper Towels" timeRemaining="5 days" />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    marginBottom: 16,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
