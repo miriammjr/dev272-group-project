31c24fcdae23ba10c4b8fc6a91ba2d65318849c3
miriammjr <mirrundell@gmail.com>
2025-06-15 21:53:06 -0700

    jest works but nothing else does

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    '@babel/preset-typescript',
+
+    [
+      '@babel/preset-env',
+
+      {
+        targets: { esmodules: false, node: 'current' },
+      },
+    ],
+  ],
+
+  plugins: [],
+};
