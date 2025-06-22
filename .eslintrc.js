70e4d49d3618bf3ac0ce884cf4e2673851a2ee66
miriammjr <mirrundell@gmail.com>
2025-06-21 23:14:26 -0700

    mild color update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  extends: ['expo', 'prettier'],
-  ignorePatterns: ['/dist/*'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'import/no-unresolved': 'off',
-    semi: ['error', 'always'], // enforces semicolons
-    quotes: ['error', 'double'], // enforces single quotes
-    'comma-dangle': ['error', 'always-multiline'], // enforces trailing commas
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
   },
-  overrides: [
-    {
-      // Test files only
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
     },
-  ],
-};
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      semi: ['warn', 'always'],
+    },
+  },
+]);
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -9,14 +9,15 @@ const tintColorDark = '#fff';
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '##F3F4F6',
+    tileBackground: '#fff',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
+    text: '#000',
     background: '#151718',
     tint: tintColorDark,
     icon: '#9BA1A6',
