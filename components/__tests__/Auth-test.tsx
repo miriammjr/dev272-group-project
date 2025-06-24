aef403a264e44c6bdecfebd687f487a2098dda22
miriammjr <mirrundell@gmail.com>
2025-06-23 21:18:39 -0700

    Revert "one more auth test, accessibility label added"
    This reverts commit 3b4c0c7d32d88fd50480403742e0f199ec617d51.

--- components/__tests__/Auth-test.tsx
+++ components/__tests__/Auth-test.tsx
@@ -77,15 +77,6 @@ describe('<Home />', () => {
     );
   });
 
-  test('Password Visibility', async () => {
-    render(<Auth />);
-
-    fireEvent.press(screen.getByAccessibilityHint('Password Hidden'));
-    await waitFor(() => {
-      expect(screen.getByAccessibilityHint('Password Visible')).toBeTruthy();
-    });
-  });
-
   // UNSURE WHY THIS DOESN'T WORK
   // test('Sign up fails if bad email/password entered', async () => {
   //   render(<Auth />);
