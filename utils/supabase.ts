ec438be7a6bf7fc778771387c493867dd897c9b3
miriammjr <mirrundell@gmail.com>
2025-06-23 21:18:48 -0700

    Revert "taskcard tests done, most of auth tests done"
    This reverts commit f7cab922a8cb1c510fb012747e8a8568e01c5da1.

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Validate environment variables
-const supabaseUrl = 'https://dgskfdjzvcqslsbymlhd.supabase.co/';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnc2tmZGp6dmNxc2xzYnltbGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MzYyMDIsImV4cCI6MjA2NDExMjIwMn0.GAWJmUjaqfzpMWElAtppPBHtjwXoS9ICspORZrx03rA';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
